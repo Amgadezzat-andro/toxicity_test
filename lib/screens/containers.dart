fed7fa56176df8fc740d6a58dfc5eedf95dd14a1
Amgadezzat-andro <megostar59@gmail.com>
2021-09-27 04:19:04 +0200

    commit

--- lib/screens/containers.dart
+++ lib/screens/containers.dart
@@ -33,18 +33,181 @@ class BuildList extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              BuildContainer(
-                'Cramer rules',
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Container(
+                    color: Colors.blueGrey.shade900,
+                    child: ExpansionTile(
+                      iconColor: Colors.amber,
+                      expandedCrossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 10, left: 5),
+                          child: Row(
+                            children: [
+                              Icon(
+                                FontAwesomeIcons.vial,
+                                color: Colors.amber,
+                              ),
+                              Expanded(
+                                child: Divider(
+                                  color: Colors.amber,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 10, bottom: 10, top: 5),
+                          child: Column(
+                            children: [
+                              Center(
+                                child: Container(
+                                  height: 30,
+                                  width: 300,
+                                  alignment: Alignment.center,
+                                  color: Colors.yellow,
+                                  child: Text(
+                                    'intermediate (Class II)',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                              Divider(),
+                              Center(
+                                child: Container(
+                                  height: 30,
+                                  width: 300,
+                                  alignment: Alignment.center,
+                                  color: Colors.green,
+                                  child: Text(
+                                    'Low (Class I)',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                              Divider(),
+                              Center(
+                                child: Container(
+                                  height: 30,
+                                  width: 300,
+                                  alignment: Alignment.center,
+                                  color: Colors.red,
+                                  child: Text(
+                                    'High (Class III)',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                              Divider(),
+                              Row(
+                                children: [
+                                  Text(
+                                    'Q1.Normal constituent of the body',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(width: 70,),
+                                  Text(
+                                    'No',
+                                    style: TextStyle(
+                                      color: Colors.green,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Divider(),
+                              Row(
+                                children: [
+                                  Text(
+                                    'Q2.Contains functional groups \nassociated with enhanced toxicity',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(width: 70,),
+                                  Text(
+                                    'No',
+                                    style: TextStyle(
+                                      color: Colors.green,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Divider(),
+                              Row(
+                                children: [
+                                  Text(
+                                    'Q3.Contains elements other than \nC,H,O,N,divalent S ',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(width: 70,),
+                                  Text(
+                                    'YES',
+                                    style: TextStyle(
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Divider(),
+                              Row(
+                                children: [
+                                  Text(
+                                    'Q4.Elements not listed in Q3 \noccurs only as a Na,K,Ca,Mg,N salt\n,sulphamate,sulphonate,\nsulphate,hydrochloride ... ',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(width: 70,),
+                                  Text(
+                                    'No',
+                                    style: TextStyle(
+                                      color: Colors.green,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  
+                                ],
+                              ),
+                              Divider(),
+                            ],
+                          ),
+                        )
+                      ],
+                      textColor: Colors.white,
+                      title: Text(
+                        'Cramer Rules',
+                        style: TextStyle(fontSize: 24, color: Colors.white),
+                      ),
+                      collapsedIconColor: Colors.white,
+                      backgroundColor: Colors.blueGrey.shade900,
+                    ),
+                  ),
+                ),
               ),
-              BuildContainer(
-                'Eye irritation',
-              ),
-              BuildContainer(
-                'DNA binding',
-              ),
-              BuildContainer(
-                'Protein binding',
-              ),
+              BuildContainer('Eye irritation', 'sdfsdafsadfsdaf'),
+              BuildContainer('DNA binding', 'aSDASDASD'),
+              BuildContainer('Protein binding', '213213'),
             ],
           ),
         ),
@@ -55,8 +218,9 @@ class BuildList extends StatelessWidget {
 
 // ignore: must_be_immutable
 class BuildContainer extends StatefulWidget {
-  BuildContainer(this.title);
+  BuildContainer(this.title, this.data);
   String title;
+  String data;
   @override
   State<StatefulWidget> createState() {
     return _BuildContainer();
@@ -97,7 +261,7 @@ class _BuildContainer extends State<BuildContainer> {
               Padding(
                 padding: const EdgeInsets.only(left: 10, bottom: 10, top: 5),
                 child: Text(
-                  'Some random data',
+                  '${widget.data}',
                   textAlign: TextAlign.start,
                   style: TextStyle(fontSize: 20, color: Colors.white),
                 ),
